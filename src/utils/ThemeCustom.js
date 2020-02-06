5b9192738b39692e9934c8312904aa2f6f581ccf
MauricioDantas <mauriciodantas.contato01@bol.com.br>
2020-02-06 19:36:44 -0300

    add folder utils

---
+++ src/utils/ThemeCustom.js
@@ -0,0 +1,12 @@
+import { blue } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    }
+  },
+});
+
+export default theme
