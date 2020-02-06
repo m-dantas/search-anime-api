b58b5882b14452ed5f92c719bb37ce53a432cc0b
MauricioDantas <mauriciodantas.contato01@bol.com.br>
2020-02-06 19:40:14 -0300

    add init archives

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container, AppBar, Button, Toolbar } from "@material-ui/core"
+import Routes from './routes'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Routes />
+      </Container>
+    </>
+  )
 }
 
 export default App;
